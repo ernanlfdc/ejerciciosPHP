45d038fee39a77936789628303b3c6eac7f21e35
Ernan <ernanlfdc@gmail.com>
2016-09-30 18:34:18 +0200

    Ejercicio01 Clase

--- ejercicio01.php
+++ ejercicio01.php
@@ -1,1 +1,30 @@
-TODO: 
+<!DOCTYPE html>
+<html><head>
+        <meta charset="UTF-8">
+        <title>Ejercicio 1 PHP</title>
+</head><body> 
+
+<h4> Array de cinco elementos inicializado en la declaración. <br>
+Se muestra por pantalla con un bucle "for".</h4>
+
+<?php
+
+//Crear un script (ejercicio01.php)
+//Crear un array ordenado de cinco elementos. Inicializarlo al declararlo.
+//Muestra su contenido en un bucle "for"
+
+$array = array(
+	'Nombre' => "Ernán",
+	'Apellidos' => "LFDC",
+	'Ciclo' => "Desarrollo de Aplicaciones Web",
+	'Curso' => "Segundo",
+	'Módulo' => "Desarrollo Web en Entorno Servidor");
+//Array inicializado en la decaración
+
+foreach ($array as $position=>$element){
+	echo $position . ":	" . $element . '<br>';
+	//Recorremos el array y lo vamos mostrando por pantalla con un bucle for
+}
+
+?>
+</body></html>

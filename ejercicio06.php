3bdbb1d22f0e23f4d999de97cb4041437510480b
Ernan <ernanlfdc@gmail.com>
2016-10-05 19:08:15 +0200

    Ej6 PHP acabado

---
+++ ejercicio06.php
@@ -0,0 +1,12 @@
+<?php
+echo '&nbsp&nbsp&nbsp&nbspTitulo:&nbsp';
+echo $_POST['titulo']; 
+echo '&nbsp&nbsp&nbsp&nbspAutor:&nbsp';
+echo $_POST['autor'];
+echo '<br>&nbsp&nbsp&nbsp&nbspEditorial:&nbsp';
+echo $_POST['editoriales'];
+echo '<br>&nbsp&nbsp&nbsp&nbsp';
+echo $_POST['envio'];
+echo '&nbsp&nbsp&nbsp&nbsp';
+echo $_POST['terminos'];
+?>
